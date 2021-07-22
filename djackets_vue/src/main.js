0975e6734f53ba9f422efe0c6bea6dcd5cc84d48
Sanjay <bishtsanjay343@gmail.com>
2021-07-22 23:51:13 +0530

    fetch products and display on homepage

--- djackets_vue/src/main.js
+++ djackets_vue/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
+import axios from 'axios';
 import App from './App.vue';
 import router from './router';
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  axios,
   render: (h) => h(App),
 }).$mount('#app');
